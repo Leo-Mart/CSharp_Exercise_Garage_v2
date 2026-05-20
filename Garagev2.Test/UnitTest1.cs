9bf9d5d3aec11b9117b75782b6b53d380ca60ccd
Martin Leo <martin.christoffer.leo@gmail.com>
2026-05-20 17:01:07 +0200

    set up test project

---
+++ Garagev2.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Garagev2.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Garagev2.Test/Utilities/Utilities.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Garagev2.Test.Utilities;
+
+public class Utilities
+{
+  [Fact]
+  public void ShouldParseValidString()
+  {
+   
+  }
+}
